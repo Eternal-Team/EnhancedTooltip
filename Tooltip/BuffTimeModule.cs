2d5f6f97a2ec346328e76da7d9217f417eed81e5
Itorius <itorius.yt@gmail.com>
2019-08-21 18:06:07 +0200

    Add buff time tooltip

---
+++ Tooltip/BuffTimeModule.cs
@@ -0,0 +1,31 @@
+﻿using BaseLibrary;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace EnhancedTooltip.Tooltip
+{
+	internal class BuffTimeModule : BaseModule
+	{
+		internal override string Name => "BuffTime";
+
+		internal override TwoColumnLine Create(Item item, DrawableTooltipLine line)
+		{
+			TimeSpan span = TimeSpan.FromSeconds(item.buffTime / 60f);
+
+			string text = "";
+			if (span.Hours > 0) text += $"{span.Hours} hour{(span.Hours > 1 ? "s" : "")} ";
+			if (span.Minutes > 0) text += $"{span.Minutes} minute{(span.Minutes > 1 ? "s" : "")} ";
+			if (span.Seconds > 0) text += $"{span.Seconds} second{(span.Seconds > 1 ? "s" : "")} ";
+			text = text.Trim();
+
+			return new TwoColumnLine(line)
+			{
+				textLeft = "Buff duration",
+				textRight = text,
+				colorRight = Utility.DoubleLerp(Color.Red, Color.Yellow, Color.LimeGreen, (float)item.buffTime / EnhancedTooltip.GetStat("BuffTime"))
+			};
+		}
+	}
+}
